2d4019d594bb696c80682b2ac4304fe89ea11c5a
zubeir <zubwd1@gmail.com>
2024-10-09 12:20:47 +0100

    update script bd en collections

---
+++ src/assets/script.sql
@@ -0,0 +1,34 @@
+CREATE DATABASE recipe_management;
+USE recipe_management;
+
+CREATE TABLE recipes (
+    id INT PRIMARY KEY AUTO_INCREMENT NOT NULL,
+    title VARCHAR(100) NOT NULL,
+    type VARCHAR(10) NOT NULL,
+    ingredients TEXT NOT NULL,
+    category_id INT NOT NULL,
+    CONSTRAINT fk_category
+    FOREIGN KEY (category_id) REFERENCES categories(id)
+    ON DELETE RESTRICT
+);
+
+CREATE TABLE categories (
+    id INT PRIMARY KEY AUTO_INCREMENT NOT NULL,
+    name TEXT NOT NULL
+);
+
+INSERT INTO categories (name) 
+VALUES 
+('Italienne'),
+('Mexicaine'),
+('Japonaise'),
+('Indienne'),
+('Française');
+
+INSERT INTO recipes (title, type, ingredients, category_id)
+VALUES 
+('Salade César', 'entrée', 'Laitue, Poulet, Croutons, Parmesan, Sauce César', 1),
+('Pâtes Carbonara', 'plat', 'Pâtes, Crème, Lardons, Parmesan, Oeufs', 2),
+('Tiramisu', 'dessert', 'Mascarpone, Café, Biscuits à la cuillère, Cacao, Oeufs', 3),
+('Salade de fruits', 'dessert', 'Pommes, Bananes, Oranges, Fraises, Jus de citron', 4),
+('Soupe à l\'oignon', 'entrée', 'Oignons, Bouillon, Pain grillé, Fromage râpé', 5);
